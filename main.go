18e357a85a51a0067d70f958d016cc4d45c744e1
matholi <matheus.oliveria2100@hotmail.com>
2021-11-15 14:16:25 -0300

    criação do Struct

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
+import (
+	"fmt"
+)
+
+type contaCorrente struct {
+	Titular string
+	Agencia int
+	Conta   int
+	Saldo   float64
+}
+
 func main() {
-	
-	var (
-		nTitular := "Matheus"
-		nAgencia := 589
-		nConta := 123456
-		saldo := 1000.00
-	)
+
+	contaMatheus := contaCorrente{
+		Titular: "Matheus",
+		Agencia: 589,
+		Conta:   123456,
+		Saldo:   1000.00,
+	}
+
+	conta := fmt.Sprintf("Nome: %s \nAg:%d \nCc: %d \nSaldo: %.2f", contaMatheus.Titular, contaMatheus.Agencia, contaMatheus.Conta, contaMatheus.Saldo)
+
+	fmt.Println(conta)
 
 }
