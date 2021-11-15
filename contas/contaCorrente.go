8d98e4dfccf8fdf279b1daa2bb6f08806de183c0
matholi <matheus.oliveria2100@hotmail.com>
2021-11-15 15:18:01 -0300

    Criação de Pacotes

---
+++ contas/contaCorrente.go
@@ -0,0 +1,44 @@
+package contas
+
+type ContaCorrente struct {
+	Titular string
+	Agencia int
+	Conta   int
+	Saldo   float64
+}
+
+func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) string {
+
+	podeTransferir := valorTransferencia <= c.Saldo && valorTransferencia > 0
+
+	if podeTransferir {
+		c.Saldo -= valorTransferencia
+		contaDestino.Saldo += valorTransferencia
+		return "Transferencia realizada com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valor float64) string {
+	podeDepositar := valor > 0
+	if podeDepositar {
+		c.Saldo += valor
+		return "Deposito realizado com sucesso"
+	} else {
+		return "Valor invalido"
+	}
+}
+
+func (c *ContaCorrente) Sacar(valorSaque float64) (string, float64) {
+
+	podeSacar := valorSaque <= c.Saldo && valorSaque > 0
+
+	if podeSacar {
+		c.Saldo -= valorSaque
+		return "Saque realizado com sucesso", c.Saldo
+	} else {
+		return "Saldo insuficiente", c.Saldo
+	}
+
+}
--- main.go
+++ main.go
@@ -1,26 +1,20 @@
 package main
 
 import (
+	"banco/contas"
 	"fmt"
 )
 
-type contaCorrente struct {
-	Titular string
-	Agencia int
-	Conta   int
-	Saldo   float64
-}
-
 func main() {
 
-	conta1 := contaCorrente{
+	conta1 := contas.ContaCorrente{
 		Titular: "Matheus",
 		Agencia: 589,
 		Conta:   123456,
 		Saldo:   1000.00,
 	}
 
-	conta2 := contaCorrente{
+	conta2 := contas.ContaCorrente{
 		Titular: "Marcela",
 		Agencia: 589,
 		Conta:   123456,
@@ -31,46 +25,10 @@ func main() {
 	fmt.Println(conta1.Saldo)
 	fmt.Println(conta2.Titular)
 	fmt.Println(conta2.Saldo)
-	fmt.Println(conta1.transferir(100, &conta2))
+	fmt.Println(conta1.Transferir(100, &conta2))
 	fmt.Println(conta1.Titular)
 	fmt.Println(conta1.Saldo)
 	fmt.Println(conta2.Titular)
 	fmt.Println(conta2.Saldo)
-
-}
-
-func (c *contaCorrente) transferir(valorTransferencia float64, contaDestino *contaCorrente) string {
-
-	podeTransferir := valorTransferencia <= c.Saldo && valorTransferencia > 0
-
-	if podeTransferir {
-		c.Saldo -= valorTransferencia
-		contaDestino.Saldo += valorTransferencia
-		return "Transferencia realizada com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
-
-func (c *contaCorrente) depositar(valor float64) string {
-	podeDepositar := valor > 0
-	if podeDepositar {
-		c.Saldo += valor
-		return "Deposito realizado com sucesso"
-	} else {
-		return "Valor invalido"
-	}
-}
-
-func (c *contaCorrente) sacar(valorSaque float64) (string, float64) {
-
-	podeSacar := valorSaque <= c.Saldo && valorSaque > 0
-
-	if podeSacar {
-		c.Saldo -= valorSaque
-		return "Saque realizado com sucesso", c.Saldo
-	} else {
-		return "Saldo insuficiente", c.Saldo
-	}
 
 }
