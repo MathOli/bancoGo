af2719d39e6b7b58d6a547eb4060f54275f66d0e
matholi <matheus.oliveria2100@hotmail.com>
2021-11-15 15:10:58 -0300

    Criação de Metodos(Sacar,Depositar e Transferir)

--- main.go
+++ main.go
@@ -13,15 +13,64 @@ type contaCorrente struct {
 
 func main() {
 
-	contaMatheus := contaCorrente{
+	conta1 := contaCorrente{
 		Titular: "Matheus",
 		Agencia: 589,
 		Conta:   123456,
 		Saldo:   1000.00,
 	}
 
-	conta := fmt.Sprintf("Nome: %s \nAg:%d \nCc: %d \nSaldo: %.2f", contaMatheus.Titular, contaMatheus.Agencia, contaMatheus.Conta, contaMatheus.Saldo)
+	conta2 := contaCorrente{
+		Titular: "Marcela",
+		Agencia: 589,
+		Conta:   123456,
+		Saldo:   1000.00,
+	}
 
-	fmt.Println(conta)
+	fmt.Println(conta1.Titular)
+	fmt.Println(conta1.Saldo)
+	fmt.Println(conta2.Titular)
+	fmt.Println(conta2.Saldo)
+	fmt.Println(conta1.transferir(100, &conta2))
+	fmt.Println(conta1.Titular)
+	fmt.Println(conta1.Saldo)
+	fmt.Println(conta2.Titular)
+	fmt.Println(conta2.Saldo)
+
+}
+
+func (c *contaCorrente) transferir(valorTransferencia float64, contaDestino *contaCorrente) string {
+
+	podeTransferir := valorTransferencia <= c.Saldo && valorTransferencia > 0
+
+	if podeTransferir {
+		c.Saldo -= valorTransferencia
+		contaDestino.Saldo += valorTransferencia
+		return "Transferencia realizada com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *contaCorrente) depositar(valor float64) string {
+	podeDepositar := valor > 0
+	if podeDepositar {
+		c.Saldo += valor
+		return "Deposito realizado com sucesso"
+	} else {
+		return "Valor invalido"
+	}
+}
+
+func (c *contaCorrente) sacar(valorSaque float64) (string, float64) {
+
+	podeSacar := valorSaque <= c.Saldo && valorSaque > 0
+
+	if podeSacar {
+		c.Saldo -= valorSaque
+		return "Saque realizado com sucesso", c.Saldo
+	} else {
+		return "Saldo insuficiente", c.Saldo
+	}
 
 }
